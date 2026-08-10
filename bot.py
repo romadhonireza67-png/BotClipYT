07b435e95d97def38092b088f0b9cbdbe98477cd
romadhonireza67-png <romadhonireza67@gmail.com>
2026-08-10 17:40:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -137,7 +137,7 @@ async def receive_time_range(update: Update, context: ContextTypes.DEFAULT_TYPE)
             return [{"start_time": start_sec, "end_time": end_sec}]
 
         ydl_opts = {
-            "format": "best[ext=mp4]/best",
+            "format": "bv*+ba/b",
             "outtmpl": output_template,
             "download_ranges": section_selector,
             "force_keyframes_at_cuts": True,
