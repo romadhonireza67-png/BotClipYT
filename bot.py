867a3f5dc84a1408f493ac0ddbc33baa650a4d8b
romadhonireza67-png <romadhonireza67@gmail.com>
2026-08-10 17:58:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,7 +147,7 @@ async def receive_time_range(update: Update, context: ContextTypes.DEFAULT_TYPE)
             "merge_output_format": "mp4",
             "extractor_args": {
                 "youtube": {
-                    "player_client": ["android", "ios", "tv", "web"],
+                    "player_client": ["android", "ios"],
                 },
                 "youtubepot-bgutilhttp": {
                     "base_url": ["http://bgutil-ytdlp-pot-provider.railway.internal:4416"]
